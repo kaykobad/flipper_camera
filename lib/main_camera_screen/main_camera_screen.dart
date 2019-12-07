e563494f239b34bc455cec3baff5fe5c3e68f02e
kaykobad <kaykobadreza@gmail.com>
2019-12-07 22:22:13 +0600

    Add image capture and preview

--- lib/main_camera_screen/main_camera_screen.dart
+++ lib/main_camera_screen/main_camera_screen.dart
@@ -1,5 +1,8 @@
+import 'package:flipper_camera/image_preview_screen/image_preview_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 class MainCameraScreen extends StatefulWidget {
   @override
@@ -7,6 +10,66 @@ class MainCameraScreen extends StatefulWidget {
 }
 
 class _MainCameraScreenState extends State<MainCameraScreen> {
+  CameraController controller;
+  List cameras;
+  int selectedCameraIdx;
+  String imagePath;
+
+  void initState() {
+    super.initState();
+
+    availableCameras().then((availableCameras) {
+      // take all the available cameras
+      cameras = availableCameras;
+
+      // if camera available, take the back camera
+      if (cameras.length > 0) {
+        print("============ ${cameras.length} ===========");
+        setState(() {
+          selectedCameraIdx = 0;
+        });
+
+        // initialize the camera controller
+        _initCameraController(cameras[selectedCameraIdx]).then((void v) {
+          print("Camera initialization done!");
+        });
+      } else {
+        print("No camera available");
+      }
+    }).catchError((err) {
+      print('Error: $err.code\nError Message: $err.message');
+    });
+  }
+
+  Future _initCameraController(CameraDescription cameraDescription) async {
+    if (controller != null) {
+      await controller.dispose();
+    }
+
+    controller = CameraController(cameraDescription, ResolutionPreset.high);
+
+    // If the controller is updated then update the UI.
+    controller.addListener(() {
+      if (mounted) {
+        setState(() {});
+      }
+
+      if (controller.value.hasError) {
+        print('Camera error ${controller.value.errorDescription}');
+      }
+    });
+
+    try {
+      await controller.initialize();
+    } on CameraException catch (e) {
+      _showCameraException(e);
+    }
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,7 +110,21 @@ class _MainCameraScreenState extends State<MainCameraScreen> {
 
   /// Display Camera preview.
   Widget _cameraPreviewWidget() {
-    return Container();
+    if (controller == null || !controller.value.isInitialized) {
+      return Text(
+        'Loading',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20.0,
+          fontWeight: FontWeight.w900,
+        ),
+      );
+    }
+
+    return AspectRatio(
+      aspectRatio: controller.value.aspectRatio,
+      child: CameraPreview(controller),
+    );
   }
 
   /// Display the control bar with buttons to take pictures
@@ -74,11 +151,64 @@ class _MainCameraScreenState extends State<MainCameraScreen> {
 
   /// Display a row of toggle to select the camera (or a message if no camera is available).
   Widget _cameraTogglesRowWidget() {
-    return Spacer();
+    if (cameras == null || cameras.isEmpty) {
+      return Spacer();
+    }
+
+    CameraDescription selectedCamera = cameras[selectedCameraIdx];
+    CameraLensDirection lensDirection = selectedCamera.lensDirection;
+
+    return Expanded(
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: FlatButton.icon(
+          onPressed: _onSwitchCamera,
+          icon: Icon(_getCameraLensIcon(lensDirection)),
+          label: Text(
+              "${lensDirection.toString().substring(lensDirection.toString().indexOf('.') + 1)}"),
+        ),
+      ),
+    );
+  }
+
+  IconData _getCameraLensIcon(CameraLensDirection direction) {
+    switch (direction) {
+      case CameraLensDirection.back:
+        return Icons.camera_rear;
+      case CameraLensDirection.front:
+        return Icons.camera_front;
+      case CameraLensDirection.external:
+        return Icons.camera;
+      default:
+        return Icons.device_unknown;
+    }
+  }
+
+  void _onSwitchCamera() {
+    selectedCameraIdx =
+    selectedCameraIdx < cameras.length - 1 ? selectedCameraIdx + 1 : 0;
+    CameraDescription selectedCamera = cameras[selectedCameraIdx];
+    _initCameraController(selectedCamera);
   }
 
   void _onCapturePressed(context) async {
+    try {
+      final path = join(
+        (await getTemporaryDirectory()).path,
+        '${DateTime.now()}.png',
+      );
 
+      await controller.takePicture(path);
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ImagePreviewScreen(imagePath: path),
+        ),
+      );
+    } catch (e) {
+      print(e);
+    }
   }
 
   void _showCameraException(CameraException e) {
